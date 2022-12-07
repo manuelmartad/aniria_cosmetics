b375a895f7b1b78b89f8d4daf89fd4382dd525be
Manuel Marta <rodolfomartad@gmail.com>
2022-12-07 10:35:38 -0700

    initial commit

--- admin/dashboard/dashboard.php
+++ admin/dashboard/dashboard.php
@@ -81,18 +81,42 @@ include '../../includes/templates/nav.php';
                 </div>
 
                 <div class="col-md-6 col-lg-4 mb-3">
-                    <div class="card p-3 shadow-lg" style="border-left: 4px solid #0f0f00;">
+                    <div class="card p-3 shadow-lg" style="border-left: 4px solid #0f0f0f;">
                         <div class="card-title text-center">
-                            <span> Productos </span>
+                            <span> Productos</span>
                         </div>
-                        <div class="card-body fs-4 text-center card-animated">
-                            <a href="<?php echo ADMIN ?>products/product_view.php">
-                                <i class='bx bx-box text-dark' style="font-size:80px;"></i>
-                                <span class="text-danger align-middle pb-5">
-                                    <?php $products =  $conn->query("SELECT count(product_id) AS products FROM products");
-                                    $d = $products->fetch_assoc();
-                                    echo $d['products']; ?>
-                                </span>
+                        <div class="card-body fs-2 text-center card-animated">
+                            <a href="<?php echo ADMIN ?>products/product_view.php" class="position-relative">
+                                <i class='bx bx-box text-dark' style="font-size:120px; font-weight:300!important"></i>
+                                <span class="position-absolute ms-1 mb-4 px-3 py-2 end-120 top-10 bottom-100 start-0 translate-middle badge rounded-pill bg-danger">
+
+                                    <span>
+                                        <?php $q =  $conn->query("SELECT count(product_id) AS product FROM products");
+                                        $d = $q->fetch_assoc();
+                                        echo $d['product']; ?>
+                                    </span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+
+
+                <div class="col-md-6 col-lg-4 mb-3">
+                    <div class="card p-3 shadow-lg" style="border-left: 4px solid #d63384;">
+                        <div class="card-title text-center">
+                            <span> Categorias</span>
+                        </div>
+                        <div class="card-body fs-2 text-center card-animated">
+                            <a href="<?php echo ADMIN ?>categories/category_view.php" class="position-relative">
+                                <i class='bx bx-category text-primary' style="font-size:120px; font-weight:300!important"></i>
+                                <span class="position-absolute ms-1 mb-4 px-3 py-2 end-120 top-10 bottom-100 start-0 translate-middle badge rounded-pill bg-danger">
+
+                                    <span>
+                                        <?php $q =  $conn->query("SELECT count(category_id) AS category FROM categories");
+                                        $d = $q->fetch_assoc();
+                                        echo $d['category']; ?>
+                                    </span>
                             </a>
                         </div>
                     </div>
